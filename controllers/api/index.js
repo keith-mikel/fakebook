7678d5a79ec2ff784e9ba252c8fa0e9e0344adc7
keith-mikel <keith.mikel@e5sol.com>
2023-08-10 20:26:26 -0500

    added bot and user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,15 @@
-const router = require("express").Router();
+const express = require('express');
+const router = express.Router();
+
 const botRoutes = require("./botRoutes");
 const commentsRoutes = require("./commentsRoutes");
-const postsRoutes = require("./postsRoutes");
-const userRoutes = require("./userRoutes");
+// const postsRoutes = require("./postsRoutes");
+// const userRoutes = require("./userRoutes");
 
 // Verify routes below
 router.use("/bots", botRoutes);
 router.use("/comments", commentsRoutes);
-router.use("/posts", postsRoutes);
-router.use("/users", userRoutes);
+// router.use("/posts", postsRoutes);
+// router.use("/users", userRoutes);
 
 module.exports = router;
